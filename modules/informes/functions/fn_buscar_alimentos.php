2d2a63f626c70018bfbcc279b0b4e78a3b0a372f
Daniel <wappsi.desarrolloweb02@gmail.com>
2019-03-29 11:37:57 -0500

    Modulo de informes
    Se agrega pagina dónde se muestra la información de alimentos.

--- modules/informes/functions/fn_buscar_alimentos.php
+++ modules/informes/functions/fn_buscar_alimentos.php
@@ -2,6 +2,7 @@
 require_once '../../../config.php';
 require_once '../../../db/conexion.php';
 
+$data = [];
 $periodo_actual = $_SESSION["periodoActual"];
 $mes = (isset($_POST['mes'])) ? $Link->real_escape_string($_POST['mes']) : "";
 $sede = (isset($_POST['sede'])) ? $Link->real_escape_string($_POST['sede']) : "";
@@ -12,37 +13,32 @@ $semana_final = (isset($_POST['semana_final'])) ? $Link->real_escape_string($_PO
 $semana_inicial = (isset($_POST['semana_inicial'])) ? $Link->real_escape_string($_POST['semana_inicial']) : "";
 $tipo_complemento = (isset($_POST['tipo_complemento'])) ? $Link->real_escape_string($_POST['tipo_complemento']) : "";
 
-$condicion_tipo_complemento = ($tipo_complemento != "") ? "AND d.Tipo_Complem = '$tipo_complemento'" : "";
+$condicion_sede = ($sede != "") ? "AND sed.cod_sede = '268013000053'" : "";
 $condicion_municipio = ($municipio != "") ? "AND sed.cod_mun_sede = '$municipio'" : "";
 $condicion_institucion = ($institucion != "") ? "AND sed.cod_inst = '168013000016'" : "";
-$condicion_sede = ($sede != "") ? "AND sed.cod_sede = '268013000053'" : "";
+$condicion_tipo_complemento = ($tipo_complemento != "") ? "AND d.Tipo_Complem = '$tipo_complemento'" : "";
+
 $consulta = "SELECT
-  pm.codigoproducto AS codigo_producto,
-  pm.descripcion AS descripcion,
+  pm.CodigoProducto AS codigo_producto,
+  pm.Descripcion AS descripcion,
   SUM(pm.CantUmedida) AS cantidad_requerida,
-  SUM(pm.cantotalpresentacion) AS cantidad_presentacion,
-  p.nombreunidad1 AS nombre_unidad_1,
-  SUM(pm.cantU2) AS cantidad_unidad_2,
-  p.nombreunidad2 AS nombre_unidad_2,
-  SUM(pm.cantU3) AS cantidad_unidad_3,
-  p.nombreunidad3 AS nombre_unidad_3,
-  SUM(pm.cantU4) AS cantidad_unidad_4,
-  p.nombreunidad4 AS nombre_unidad_4,
-  SUM(pm.cantU5) AS cantidad_unidad_5,
-  p.nombreunidad5 AS nombre_cantidad_5
+  CAST(SUM(pm.CanTotalPresentacion) AS DECIMAL(12,2)) AS cantidad_presentacion,
+  CAST(SUM(pm.CantU1) AS DECIMAL(12,2)) AS cantidad_unidad_1,
+  p.NombreUnidad1 AS nombre_unidad_1,
+  CAST(SUM(pm.CantU2) AS DECIMAL(12,2)) AS cantidad_unidad_2,
+  p.NombreUnidad2 AS nombre_unidad_2,
+  CAST(SUM(pm.CantU3) AS DECIMAL(12,2)) AS cantidad_unidad_3,
+  p.NombreUnidad3 AS nombre_unidad_3,
+  CAST(SUM(pm.CantU4) AS DECIMAL(12,2)) AS cantidad_unidad_4,
+  p.NombreUnidad4 AS nombre_unidad_4
 FROM productosmovdet$mes$periodo_actual pm
 STRAIGHT_JOIN productosmov$mes$periodo_actual prm ON pm.Numero = prm.Numero
-INNER JOIN productos$mes p ON (pm.codigoProducto=p.codigo)
+INNER JOIN productos$periodo_actual p ON pm.CodigoProducto = p.codigo
 INNER JOIN sedes19 sed ON sed.cod_sede = pm.BodegaDestino
-WHERE pm.numero IN (SELECT num_doc FROM despachos_enc$mes$periodo_actual d WHERE d.estado<> 0 AND semana BETWEEN '$semana_inicial' AND '$semana_final' $condicion_tipo_complemento)
-GROUP BY pm.codigoproducto";
-// AND sed.cod_mun_sede = '68013'
-// AND sed.cod_inst = '168013000016'
-// AND sed.cod_sede = '268013000053'
+WHERE pm.Numero IN (SELECT num_doc FROM despachos_enc$mes$periodo_actual d WHERE d.estado<> 0 AND semana BETWEEN '$semana_inicial' AND '$semana_final' $condicion_tipo_complemento)
+GROUP BY pm.CodigoProducto";
 
-$data = [];
-
-$respuesta = $Link->query($consulta);
+$respuesta = $Link->query($consulta) or die("Error al consultar alimentos: ". $Link->error);
 if ($respuesta->num_rows > 0) {
 	while ($datos = $respuesta->fetch_assoc()) {
 		$data[] = $datos;
