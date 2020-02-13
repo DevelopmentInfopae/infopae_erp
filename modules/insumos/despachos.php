9332f28f02a7c939e2199b26d9bd40ef6e0c3cdf
Daniel Torres <wappsi.desarrolloweb02@gmail.com>
2020-02-13 11:47:54 -0500

    Modulo despachos (Corrección)
    Se corrige la consulta para despachos de insumos.
    Se agrega mensaje de error cuando el despacho ya se encuentra generado para dicha sede.

--- modules/insumos/despachos.php
+++ modules/insumos/despachos.php
@@ -291,24 +291,13 @@
 
     $consulta = "";
 
-    // $consulta = "SELECT
-    //                  pmov.Tipo, pmov.Numero, u.Ciudad, pmov.Aprobado, pmov.FechaMYSQL, bodegas.NOMBRE as nomBodegaOrigen, b2.NOMBRE as nomBodegaDestino, pmov.Id, pmov.BodegaDestino, sede.cod_inst
-    //               FROM
-    //                 insumosmov$numtabla AS pmov
-    //                   INNER JOIN bodegas ON bodegas.ID = pmov.BodegaOrigen
-    //                   INNER JOIN bodegas as b2 ON b2.ID = pmov.BodegaDestino
-    //                   INNER JOIN tipovehiculo ON tipovehiculo.Id = pmov.TipoTransporte
-    //                   $inners $condiciones
-    //                   LEFT JOIN ubicacion as u ON u.codigoDANE = sede.cod_mun_sede
-    //               LIMIT 2000;";
-
     for ($i=$num_mes_inicio; $i <= $num_mes_fin ; $i++) {
        $consulta.="SELECT
-                     pmov.Tipo, pmov.Numero, u.Ciudad, pmov.Aprobado, pmov.FechaMYSQL, bodegas.NOMBRE as nomBodegaOrigen, b2.NOMBRE as nomBodegaDestino, pmov.Id, pmov.BodegaDestino, sede.cod_inst, '".($i < 10 ? "0".$i : $i)."' as mesDespacho
+                     pmov.Tipo, pmov.Numero, u.Ciudad, pmov.Aprobado, pmov.FechaMYSQL, bodegas.NOMBRE as nomBodegaOrigen, IF (b2.NOMBRE IS NULL, 'No existe bodega', b2.NOMBRE) as nomBodegaDestino, pmov.Id, pmov.BodegaDestino, sede.cod_inst, '".($i < 10 ? "0".$i : $i)."' as mesDespacho
                   FROM
                     insumosmov".($i < 10 ? "0".$i : $i).$_SESSION['periodoActual']." AS pmov
                       INNER JOIN bodegas ON bodegas.ID = pmov.BodegaOrigen
-                      INNER JOIN bodegas as b2 ON b2.ID = pmov.BodegaDestino
+                      LEFT JOIN bodegas as b2 ON b2.ID = pmov.BodegaDestino
                       INNER JOIN tipovehiculo ON tipovehiculo.Id = pmov.TipoTransporte
                       $inners $condiciones
                       LEFT JOIN ubicacion as u ON u.codigoDANE = sede.cod_mun_sede
@@ -318,15 +307,13 @@
 
       $consulta = trim($consulta, "UNION ALL ");
 }
-
-// echo $consulta;
 ?>
               <input type="hidden" name="consulta" id="consulta" value="<?php echo $consulta; ?>">
-        </div><!-- /.ibox-content -->
-      </div><!-- /.ibox float-e-margins -->
-    </div><!-- /.col-lg-12 -->
-  </div><!-- /.row -->
-</div><!-- /.wrapper wrapper-content animated fadeInRight -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <form method="POST" action="editar_despacho_insumo.php" id="editar_despacho">
   <input type="hidden" name="id_despacho" id="id_despacho">
@@ -373,8 +360,7 @@
 <!-- Section Scripts -->
 <script src="<?php echo $baseUrl; ?>/modules/insumos/js/despachos.js"></script>
 
-  <script type="text/javascript">
-  console.log('Aplicando Data Table');
+<script type="text/javascript">
   dataset1 = $('#tablaTrazabilidad').DataTable({
     ajax: {
         method: 'POST',
