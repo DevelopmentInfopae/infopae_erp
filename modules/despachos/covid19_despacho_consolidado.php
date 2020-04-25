76edf4dd7841e8c800648d1429f8cd67674f9588
Ricardo Farfán <ricardo@xlogam.com>
2020-04-25 11:39:39 -0500

    Ajustes Consolidado COVID19 - Formato Rector COVID19

--- modules/despachos/covid19_despacho_consolidado.php
+++ modules/despachos/covid19_despacho_consolidado.php
@@ -287,8 +287,8 @@ class PDF extends PDF_PageGroup{
 		$this->Cell(33,4,utf8_decode("Firma de quien entrega la RPC:"),0,0,'L',False);
 		$this->Cell(67,4,utf8_decode(""),'B',0,'C',False);
 		$this->Cell(43,4,utf8_decode(""),0,0,'C',False);
-		$this->Cell(64,4,utf8_decode("Firma del responsable de la ETC (supervisión / interventoria):"),0,0,'L',False);
-		$this->Cell(67,4,utf8_decode(""),'B',0,'C',False);
+		$this->Cell(38,4,utf8_decode("Firma Rector o Representante CAE:"),0,0,'L',False);
+		$this->Cell(93,4,utf8_decode(""),'B',0,'C',False);
 		
 		$this->Ln(7);
 		$this->Cell(35,4,utf8_decode("Nombre legible de quien entrega:"),0,0,'L',False);
@@ -308,8 +308,8 @@ class PDF extends PDF_PageGroup{
 		
 		
 		
-		$this->Cell(23,4,utf8_decode("Cargo / función ETC:"),0,0,'L',False);
-		$this->Cell(36,4,utf8_decode(""),'B',0,'C',False);
+		$this->Cell(18,4,utf8_decode("Cargo / función:"),0,0,'L',False);
+		$this->Cell(41,4,utf8_decode(""),'B',0,'C',False);
 		
 		$this->Cell(13,4,utf8_decode(""),0,0,'C',False);
 		
