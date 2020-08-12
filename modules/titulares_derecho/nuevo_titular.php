3da02701d5a2917bdc6d2c9bd9c2cb9efbd885cf
Henry <wappsi.desarrolloweb01@gmail.com>
2020-08-12 15:13:29 -0500

    Modificación titulares

--- modules/titulares_derecho/nuevo_titular.php
+++ modules/titulares_derecho/nuevo_titular.php
@@ -206,6 +206,12 @@ if ($_SESSION['perfil'] == 1 || $_SESSION['perfil'] == 0) {
                   <input type="number" name="tel_acudiente" id="tel_acudiente" class="form-control" min="0" required>
                   <label for="tel_acudiente" class="error"></label>
                 </div>
+
+                <div class="form-group col-sm-3">
+                  <label>Parentesco acudiente</label>
+                  <input type="text" name="parantesco_acudiente" id="parantesco_acudiente" class="form-control" required>
+                  <label for="parantesco_acudiente" class="error"></label>
+                </div>
               </section>
               <h3>Información especial</h3>
               <section>
@@ -488,9 +494,7 @@ form.children("div").steps({
     }
     });*/
 
-    $('.select2').select2({
-      width: "resolve"
-    });
+    $('.form-group').find('select.form-control').select2({width: "100%"});
 </script>
 
 <?php mysqli_close($Link); ?>
